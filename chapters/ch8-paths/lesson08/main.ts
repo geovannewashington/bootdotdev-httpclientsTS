d273aaff26b1727dae3e149bc8c1414875320fc1
Geovanne Washington <geoxp98@gmail.com>
2025-12-09 12:52:16 -0300

    ch8: add lesson n.8 solution

---
+++ chapters/ch8-paths/lesson08/main.ts
@@ -0,0 +1,68 @@
+import { Availability, Issue } from "./types";
+
+async function fetchTasks(
+  baseURL: string,
+  availability: Availability,
+): Promise<Issue[]> {
+    let limit;
+    switch (availability.toLowerCase()) {
+        case "low": 
+            limit = 1;
+            break;
+        case "medium": 
+            limit = 3;
+            break;
+        case "high": 
+            limit = 5;
+            break;
+        default: 
+            limit = 0;
+    }
+    const fullURL = `${baseURL}?sort=estimate&limit=${limit}`;
+    return getIssues(fullURL, apiKey);
+}
+
+// don't touch below this line
+
+const url = "https://api.boot.dev/v1/courses_rest_api/learn-http/issues";
+const apiKey = generateKey();
+
+const lowIssues = await fetchTasks(url, "Low");
+console.log("Getting issues for low availability user...");
+for (const issue of lowIssues) {
+  console.log(`Issue: ${issue.title} - estimate: ${issue.estimate}`);
+}
+console.log("---");
+
+const mediumIssues = await fetchTasks(url, "Medium");
+console.log("Getting issues for medium availability user...");
+for (const issue of mediumIssues) {
+  console.log(`Issue: ${issue.title} - estimate: ${issue.estimate}`);
+}
+console.log("---");
+
+console.log("Getting issues for high availability user...");
+const highIssues = await fetchTasks(url, "High");
+for (const issue of highIssues) {
+  console.log(`Issue: ${issue.title} - estimate: ${issue.estimate}`);
+}
+
+async function getIssues(url: string, apiKey: string): Promise<Issue[]> {
+  const response = await fetch(url, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "X-API-Key": apiKey,
+    },
+  });
+  return response.json();
+}
+
+function generateKey(): string {
+  const characters = "ABCDEF0123456789";
+  let result = "";
+  for (let i = 0; i < 16; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
